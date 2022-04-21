66ba9603c9103492dc18f837a3006065d85039a6
juliocnsouzadev <juliocnsouzadev@gmail.com>
2022-04-21 08:08:31 -0300

    add leader electtion initialization

--- app/main.go
+++ app/main.go
@@ -1,23 +1,73 @@
 package main
 
 import (
+	"context"
 	clientv3 "go.etcd.io/etcd/client/v3"
-	"time"
 	"log"
 	"os"
+	"time"
 )
 
+const LEADER = "leader"
+
 func main() {
-	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"localhost:2379", "localhost:22379", "localhost:32379"},
+	client, errCli := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"Etcd:2379", "Etcd:2380"},
 		DialTimeout: 5 * time.Second,
 	})
-	if err != nil {
-		log.Fatal(err)
+	if errCli != nil {
+		log.Fatal(errCli)
 	}
+	defer client.Close()
+
 	node := os.Getenv("NODE_NAME")
 
-	log.Printf("node: %s", node)
+	if node == "" {
+		log.Fatal("NODE_NAME is not set")
+		return
+	}
 
-	defer cli.Close()
+	leaseResponse, errGrant := client.Grant(context.TODO(), 5)
+	if errGrant != nil {
+		log.Fatal(errGrant)
+	}
+
+	for true {
+		isLeader := leaderElection(client, node, leaseResponse)
+
+		if isLeader {
+			log.Printf("\nLeader [%s]", node)
+		} else {
+			log.Printf("\nFollower [%s]", node)
+		}
+		time.Sleep(time.Second * 5)
+	}
+
+}
+
+func leaderElection(client *clientv3.Client, node string, leaseResponse *clientv3.LeaseGrantResponse) bool {
+	lease := clientv3.WithLease(leaseResponse.ID)
+
+	leader, errLeader := client.Get(context.TODO(), LEADER)
+	if errLeader != nil {
+		client.Revoke(context.TODO(), leaseResponse.ID)
+		return false
+	}
+
+	if leader.Count <= 0 {
+		_, errPut := client.Put(context.TODO(), LEADER, node, lease)
+		if errPut != nil {
+			client.Revoke(context.TODO(), leaseResponse.ID)
+			return false
+		}
+		return true
+	}
+
+	for _, keyValue := range leader.Kvs {
+		log.Printf("\n-> data %s", keyValue)
+
+	}
+
+	return false
+
 }
